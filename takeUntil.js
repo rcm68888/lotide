40b90aedd81f92574b608f1d67b07ec570818f16
Rcm <rcm68888@gmail.com>
2023-11-26 11:25:03 -0700

    First commit

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const takeUntil = (array, callback) => {
+  let newArray = [];
+  for (let item in array) {
+    if (callback(array[item]) === true){
+      break;
+    } else {
+      newArray.push(array[item])
+    }
+  } return newArray
+}
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+//console.log(results1);
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+//console.log(results2);
+
+const eqArrays = function (actual, expected){
+  let match = true
+  for(let i =0; i < actual.length; i ++){
+  if (actual[i] !== expected[i]) {
+    match = false   
+  }  
+} return match
+
+}
+const assertArrayEqual = function(actual, expected) {
+  let equivalentArrays = eqArrays(actual, expected);
+  if (equivalentArrays === true) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (equivalentArrays ===false) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArrayEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+assertArrayEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
